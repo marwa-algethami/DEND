fc1ed213242acaabc493758dc11f30ed3d315c9d
Marwa <Marwa2061@gmail.com>
2020-10-02 22:24:19 +0300

    data modeling project

---
+++ etl.py
@@ -0,0 +1,107 @@
+import os
+import glob
+import psycopg2
+import pandas as pd
+from sql_queries import *
+
+
+def process_song_file(cur, filepath):
+    '''This function will read the song_data json files and insert the records into Song & Artists tables. 
+    Parameters: 
+    cur: cursor used to execute queries.
+    filepath: already assigned in main function, songs files path.
+    '''
+    df =  pd.read_json(filepath, lines=True)
+
+    song_data = df[['song_id', 'title', 'artist_id', 'year', 'duration']]
+    song_data = song_data.values[0]
+    song_data = song_data.tolist()
+    cur.execute(song_table_insert, song_data)
+    
+    artist_data = df[['artist_id', 'artist_name', 'artist_location', 'artist_latitude',  'artist_longitude']].values[0]
+    artist_data = artist_data.tolist()
+    cur.execute(artist_table_insert, artist_data)
+
+
+def process_log_file(cur, filepath):
+    '''This function will read the log_data json files and insert the records into Time & Users and songplays tables.
+    Parameters: 
+    cur: cursor used to execute queries.
+    filepath: already assigned in main function, log files path.
+    '''
+    
+    df = pd.read_json(filepath, lines=True)
+
+    df = df.loc[lambda x: x['page'] == 'NextSong']
+
+
+    t = pd.to_datetime(df['ts'], unit='ms')
+    
+    time_data =  [ df['ts'] , t.dt.hour , t.dt.day ,t.dt.weekofyear , t.dt.month ,t.dt.year , t.dt.dayofweek]
+    column_labels =  [ 'timestamp','hour', 'day', 'weekofyear', 'month', 'year',  'weekday' ]
+    time_df = pd.DataFrame({column_labels[i]: time_data[i] for i in range(len(column_labels))} )
+
+    for i, row in time_df.iterrows():
+        cur.execute(time_table_insert, list(row))
+
+    user_df = df[['userId','firstName' , 'lastName' ,'gender','level']]
+
+    for i, row in user_df.iterrows():
+        cur.execute(user_table_insert, row)
+
+    for index, row in df.iterrows():
+        
+        cur.execute(song_select, (row.song, row.artist, row.length))
+        results = cur.fetchone()
+        
+        if results:
+            songid, artistid = results
+        else:
+            songid, artistid = None, None
+
+        # insert songplay record
+        songplay_data =  (index, row.ts, row.userId, row.level, songid, artistid, row.sessionId, row.location, row.userAgent)
+        cur.execute(songplay_table_insert, songplay_data)
+
+
+def process_data(cur, conn, filepath, func):
+    '''This function will generate the file names in a directory,  os.walk() returns a list of three items, name of the root directory,
+     a list of the names of the subdirectories, and a list of the filenames in the current directory.
+    Parameters: 
+    cur: used to execute queries.
+    filepath: for json files directories.
+    func: used in iteration process. 
+    '''
+    all_files = []
+    for root, dirs, files in os.walk(filepath):
+        files = glob.glob(os.path.join(root,'*.json'))
+        for f in files :
+            all_files.append(os.path.abspath(f))
+
+    # get total number of files found
+    num_files = len(all_files)
+    print('{} files found in {}'.format(num_files, filepath))
+
+    # iterate over files and process
+    for i, datafile in enumerate(all_files, 1):
+        func(cur, datafile)
+        conn.commit()
+        print('{}/{} files processed.'.format(i, num_files))
+
+
+def main():
+    '''This function is used in connecting to the database and calling two different functions with thier parameters. '''
+        
+    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    cur = conn.cursor()
+
+    process_data(cur, conn, filepath='data/song_data', func=process_song_file)
+    process_data(cur, conn, filepath='data/log_data', func=process_log_file)
+    
+
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ sql_queries.py
@@ -0,0 +1,85 @@
+# DROP TABLES
+
+songplay_table_drop = "DROP TABLE IF EXISTS songplays ; "
+user_table_drop = "DROP TABLE IF EXISTS users ;"
+song_table_drop = "DROP TABLE IF EXISTS songs ; "
+artist_table_drop = "DROP TABLE IF EXISTS artists ;"
+time_table_drop = "DROP TABLE IF EXISTS time; "
+
+temp_table_drop = (""" DROP TABLE IF EXISTS temp;""")
+
+# CREATE TABLES
+#specify data type and constraints 
+songplay_table_create = (""" CREATE TABLE IF NOT EXISTS songplays (
+                                                                    songplay_id serial  NOT NULL  , 
+                                                                    start_time bigint REFERENCES time,
+                                                                    user_id varchar  REFERENCES users ,
+                                                                    level varchar , 
+                                                                    song_id varchar REFERENCES songs, 
+                                                                    artist_id varchar  REFERENCES artists ,
+                                                                    session_id varchar NOT NULL ,
+                                                                    location varchar , 
+                                                                    user_agent varchar ,
+                                                                    PRIMARY KEY (songplay_id )
+                                                                 
+                                                                    
+                                                                    
+                                                                    );""")
+
+user_table_create = (""" CREATE TABLE IF NOT EXISTS users (user_id varchar  NOT NULL  , first_name varchar, last_name varchar, gender varchar, level varchar , CONSTRAINT user_pkey PRIMARY KEY (user_id) 
+);""")
+
+song_table_create = (""" CREATE TABLE IF NOT EXISTS songs (song_id varchar NOT NULL , title varchar, artist_id varchar, year int , duration float , CONSTRAINT song_pkey PRIMARY KEY (song_id) 
+);""")
+
+artist_table_create = (""" CREATE TABLE IF NOT EXISTS artists (artist_id varchar NOT NULL , name varchar, location varchar, latitude float, longitude float , CONSTRAINT artist_pkey PRIMARY KEY (artist_id) 
+);""")
+
+time_table_create = (""" CREATE TABLE IF NOT EXISTS time(start_time bigint  , hour int,day int, week int,month int , year int ,weekday int 
+,CONSTRAINT time_pkey PRIMARY KEY (start_time) 
+
+);""")
+
+#temp_table_create = (""" CREATE TABLE temp ( data json)""")
+
+
+# INSERT RECORDS
+
+songplay_table_insert = (""" INSERT INTO songplays (songplay_id ,start_time , user_id , level  , song_id , artist_id  , session_id  , location  , user_agent )
+VALUES (%s, %s, %s ,%s ,%s ,%s ,%s ,%s ,%s)  ON CONFLICT (songplay_id )
+DO NOTHING
+""")
+#i use upsert feature to update users table with new inserted level 
+user_table_insert = (""" INSERT INTO users (user_id , first_name , last_name , gender , level ) VALUES (%s, %s, %s ,%s ,%s)
+ON CONFLICT ON CONSTRAINT user_pkey
+DO 
+   UPDATE SET level = EXCLUDED.level""")
+
+song_table_insert = (""" INSERT INTO songs (song_id , title , artist_id , year  , duration ) VALUES (%s, %s, %s ,%s ,%s)
+ON CONFLICT ON CONSTRAINT song_pkey 
+DO NOTHING
+
+ """)
+
+artist_table_insert = (""" INSERT INTO artists (artist_id , name , location , latitude , longitude  ) VALUES (%s, %s, %s ,%s ,%s)
+ON CONFLICT ON CONSTRAINT artist_pkey 
+DO NOTHING
+""")
+
+
+time_table_insert = (""" INSERT INTO time (start_time , hour , day , week , month  , year  , weekday ) VALUES (%s, %s, %s ,%s ,%s ,%s ,%s)
+ON CONFLICT ON CONSTRAINT time_pkey 
+DO NOTHING""")
+
+json_copy = ("""COPY temp FROM '%s';""")
+
+# FIND SONGS
+#Implement the song_select to find the song ID and artist ID based on the title, artist name, and duration of a song.
+#Escape query values by using the placholder %s method
+song_select = ("""  select s.song_id , a.artist_id from songs as s join artists as a  on s.artist_id = a.artist_id 
+where s.title = %s and a.name = %s and s.duration = %s  """)
+
+# QUERY LISTS
+
+create_table_queries = [user_table_create, song_table_create, artist_table_create, time_table_create , songplay_table_create ,temp_table_create]
+drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop , temp_table_drop]
